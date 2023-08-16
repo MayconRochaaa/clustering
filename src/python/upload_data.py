12829abb0822795f53b48cf712cb148a817fc952
Maycon Rocha <mayconrochads@gmail.com>
2023-08-16 10:35:46 -0300

    ...

--- src/python/upload_data.py
+++ src/python/upload_data.py
@@ -10,7 +10,7 @@ port = ''
 str_connection = 'mysql+pymysql:///{user}:{psw}@{host}:{port}' '''
 
 #Endereços dos objetos e subpastas
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)) )) #Pega o endereço da pasta mãe (ProjetoOlist)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(''))) #Pega o endereço da pasta mãe (ProjetoOlist)
 DATA_DIR = os.path.join(BASE_DIR, 'data') #Cria o caminho para pasta "data" a partir da endereço da pasta mãe
 
 #Encontrando os arquivos de dados
@@ -21,11 +21,17 @@ files_names = [i for i in os.listdir(DATA_DIR) if i.endswith('.csv')]
 
 #Abrindo a conexão com o banco
 str_connection = 'sqlite:///{path_to_data}'
-str_connection = str_connection.format(path_to_data=os.path.join(DATA_DIR, 'olist.db')) 
+str_connection = str_connection.format(path_to_data=os.path.join(DATA_DIR, 'ecommerce.db')) 
 connection = sqlalchemy.create_engine(str_connection) #Faz a conexão com o banco de dados
 
 #Para cada arquivo é realizado uma inserção no banco
-for i in files_names:  #passa o caminho de cada arquivo CSV
-    df_tmp = pd.read_csv(os.path.join(DATA_DIR,i))
-    table_name = 'tb_'+i.strip('.csv').replace('olist_','').replace('_dataset','')
-    df_tmp.to_sql(table_name,connection)
+#for i in files_names:  #passa o caminho de cada arquivo CSV
+    #df_tmp = pd.read_csv(os.path.join(DATA_DIR,i))
+    #table_name = 'tb_'+i.strip('.csv').replace('-','_')
+    #df_tmp.to_sql(table_name,connection)
+
+chunk_size = 100000  # Número de linhas por lote
+for file_name in files_names:
+    table_name = 'tb_' + file_name.strip('.csv').replace('-', '_')
+    for chunk in pd.read_csv(os.path.join(DATA_DIR, file_name), chunksize=chunk_size):
+        chunk.to_sql(table_name, connection, if_exists='append')
